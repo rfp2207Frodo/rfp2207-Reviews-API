e6c18616bd74db9e25d10605851d5b48bbc0edcd
Nooby <dan.t.tran505@gmail.com>
2022-09-19 10:23:12 -0700

    added listener for server close to close all database connections

--- server/index.js
+++ server/index.js
@@ -2,48 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
-// const { get } = require('./controllers/index.js');
 const router = require('./router.js');
-/* IMPORT MODELS AND CONTROLLERS */
-
-/* SET UP MIDDLEWARE */
+const db = require('./db.js');
 
 app.use(express.json());
 
-/* SET UP ROUTES */
-
 app.use('/reviews', router);
 
-/*
-
-app.get('/reviews', (req, res) => {
-  // console.log(req);
-  // let { product_id, sort, page, count } = req.query;
-  // console.log(product_id);
-  // console.log(sort);
-  // console.log(page);
-  // console.log(count);
-  res.sendStatus(501);
-});
-
-app.get('/reviews/meta', (req, res) => {
-  res.sendStatus(501);
-});
-
-app.post('/reviews', (req, res) => {
-  res.sendStatus(501);
-});
-
-app.put('/reviews/:review_id/helpful', (req, res) => {
-  res.sendStatus(501);
-});
-
-app.put('/reviews/:review_id/report', (req, res) => {
-  res.sendStatus(501);
-});
-
-*/
-
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+process.on('SIGTERM', () => (
+  server.close(async() => {
+    await db.end();
+    console.log('Server closed SIGTERM')
+  })
+));
+
+process.on('SIGINT', () => (
+  server.close(async() => {
+    await db.end();
+    console.log('Server closed SIGINT')
+  })
+));
